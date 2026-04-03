5f350b869df975c9a62bf1c745ca166d04a6a89f
이희연 HeeYeon Lee <dgc394722@gmail.com>
2026-04-03 09:38:21 +0900

    Update token-jandi to v0.5.0

--- Casks/token-jandi.rb
+++ Casks/token-jandi.rb
@@ -1,6 +1,6 @@
 cask "token-jandi" do
-  version "0.4.0"
-  sha256 "0fad3e0350903a1f1f41c1c852c4917a550535eb89a0133bff6ac645e8d0d8fa"
+  version "0.5.0"
+  sha256 "afb2b0e8a69b0d721c3ad6d7954edae421195b66677f85fef85e632f7c3bf1d5"
 
   url "https://github.com/wheon06/token-jandi/releases/download/v#{version}/Token.Jandi-#{version}.zip"
   name "Token Jandi"
